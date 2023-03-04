b366b5ea5d2425bb544ca0abafddb2019ed93325
moefloe <bmohamed_2@hotmail.com>
2023-03-04 01:54:20 -0700

    Batch of changes to clean up html codebase and add new JS functionality
    -Changed the footer to add photo credits link
    
    -Edited background image so it shows better on smaller displays
    
    -Shortened bg image link name
    
    -Removed all onClick functions from the HTML and added them to the JS file instead
    
    -Added a function to compare the scores and turn the higher score in green color

--- index.js
+++ index.js
@@ -1,39 +1,64 @@
 // Creating variables that call on the scoreline
 
-let homeScore = document.querySelector('.home-score')
-let guestScore = document.querySelector('.guest-score')
+const homeScore = document.querySelector('.home-score')
+const guestScore = document.querySelector('.guest-score')
+const homePlusBtn = document.getElementById('home-plus-btn')
+const homeMinusBtn = document.getElementById('home-minus-btn')
+const guestPlusBtn = document.getElementById('guest-plus-btn')
+const guestMinusBtn = document.getElementById('guest-minus-btn')
 
 let homeCount = 0
 let guestCount = 0
 
-//Creating the onClick functions for the buttons
-
-function homePlus() {
-    homeCount += 1
-    homeScore.textContent = homeCount
+//Here we're creating a function to compare the scores and turn the higher score green
+function turnScoreGreen() {
+    if (homeCount > guestCount) {
+        homeScore.style.color = '#1af51a'
+        guestScore.style.color = 'white'
+    } else if (guestCount > homeCount) {
+        guestScore.style.color = '#1af51a'
+        homeScore.style.color = 'white'
+    } else {
+        guestScore.style.color = 'white'
+        homeScore.style.color = 'white'
+    }
 }
 
-function homeMinus() {
+//Creating the onClick functions for the buttons to increment or decrease the score
+
+homePlusBtn.addEventListener('click', function () {
+    homeCount += 1
+    homeScore.textContent = homeCount
+    //Next we're comparing the scores and turning the higher score green accordingly
+    turnScoreGreen()
+})
+
+homeMinusBtn.addEventListener('click', function () {
     homeCount -= 1
+
+    //We need to make sure the score does not display a negative number
+
     if (homeCount >= 0) {
         homeScore.textContent = homeCount
     } else {
         homeCount = 0
     }
-}
+    turnScoreGreen()
+})
 
-function guestPlus() {
+guestPlusBtn.addEventListener('click', function () {
     guestCount += 1
     guestScore.textContent = guestCount
-}
+    turnScoreGreen()
+})
 
-function guestMinus() {
+guestMinusBtn.addEventListener('click', function () {
 
     guestCount -= 1
-
     if (guestCount >= 0) {
         guestScore.textContent = guestCount
     } else {
         guestCount = 0
     }
-}
+    turnScoreGreen()
+})
